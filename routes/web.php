1f3f20b593ffc55a9aadaa3ed06dbb2af347ab45
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-05-15 21:31:45 +0630

    Wildcard Endpoints

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get("/hello", function () {
         ->header("Content-Type", "text/plain")
         ->header("foo", "bar");
 });
+
+Route::get("/posts/{id}", function ($id) {
+    return response("Post $id");
+});
