1255849620b0e4c5523d4092b7cd6c4e6351cdde
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2023-05-15 21:27:46 +0630

    Routing & Responses

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/hello", function () {
+    return response("<h1>Hello World</h1>", 200)
+        ->header("Content-Type", "text/plain")
+        ->header("foo", "bar");
+});
